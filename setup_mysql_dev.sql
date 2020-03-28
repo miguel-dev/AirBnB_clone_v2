2711d422320fa07ede943a336890df21492c6214
MichaelAuditore <michael_v613@hotmail.com>
2020-03-28 14:57:42 -0500

    [New files] SQL configuration statement

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- SQL statement to prepare a MySQL server
+-- for the project creating a dev user an a dev db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
