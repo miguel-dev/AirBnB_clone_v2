e9c31bc6bd764c21580ef51d097d047f40377ceb
miguel-dev <miguelacortes@zoho.com>
2020-04-02 17:45:02 +0000

    [Fixes] Skip for db storage in save function for db storage test file

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,6 +5,9 @@ import unittest
 
 class TestDBSTorage(unittest.TestCase):
     """Class for testing DB Storage"""
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db",
+                     "Skip if db storage is enabled")
     def test_save(self):
         """Tests save"""
         pass
