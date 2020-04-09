18307027bf4f0f0a6380a9dd9b7f50af7cdc7b81
MichaelAuditore <michael_v613@hotmail.com>
2020-04-09 15:28:17 -0500

    [New file] Script to deploy web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared/
+echo "<html>
+   <head>
+   </head>
+   <body>
+      Holberton School is the law
+   </body>
+ </html" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data
+sed -i '43i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\t autoindex on;\n\t}\n' /etc/nginx/sites-available/default
+service nginx reload
+exit 0
