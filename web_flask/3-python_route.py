ea6862c33a55288758e69382ef206f940817185b
miguel-dev <miguelacortes@zoho.com>
2020-04-22 02:49:50 +0000

    [Feature] 3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,8 +23,9 @@ def c_is(text):
     return 'C %s' % text.replace("_", " ")
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is(text):
+def python_is(text='is cool'):
     """Displays Python followed by the value of the text variable"""
     return 'Python %s' % text.replace("_", " ")
 
